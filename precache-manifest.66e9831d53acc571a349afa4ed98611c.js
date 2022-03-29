f29ce004f310c45cc07fad719e51f4d6f76dc7be
Munaa8822 <munaa.tsetsegmaa@gmail.com>
2022-03-29 19:18:29 +0800

    burger app

---
+++ precache-manifest.66e9831d53acc571a349afa4ed98611c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95b6543ec0a891cdfb8dff5d2fbcf0a",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "c45de8ae600c23f76b1d",
+    "url": "/burger/static/css/main.0537fc73.chunk.css"
+  },
+  {
+    "revision": "ade1f0d3cb3c4958fc86",
+    "url": "/burger/static/js/2.1831de01.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.1831de01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45de8ae600c23f76b1d",
+    "url": "/burger/static/js/main.7e65af31.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
